f5cf1667d7e792583d49548612362e01be195c9c
karan-technous <karan@technous.in>
2026-05-11 15:08:05 +0530

    Update apiBaseUrl to production environment

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const Environment = {
   production: false,
   // apiBaseUrl: 'http://localhost:9186'
-  apiBaseUrl: 'https://cafe-manage-ph7q.onrender.com'
+  apiBaseUrl: 'https://cafe-manage-production.up.railway.app'
 };
